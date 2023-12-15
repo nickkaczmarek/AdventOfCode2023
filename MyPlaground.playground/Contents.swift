98a973122d13abcff3b180f75719aa32b265e1a7
Nick Kaczmarek <nickkaczmarek@me.com>
2023-12-15 10:39:14 -0500

    Adding playground. - nk
    [main]

--- MyPlaground.playground/Contents.swift
+++ MyPlaground.playground/Contents.swift
@@ -1,3 +1,64 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+enum Colors: String, RawRepresentable, CustomStringConvertible, CaseIterable {
+    case red, green, blue
+
+    var description: String {
+        self.rawValue
+    }
+}
+
+//struct Outcome: Equatable, Comparable {
+//    static func < (lhs: Outcome, rhs: Outcome) -> Bool {
+//        lhs.color == rhs.color &&
+//        lhs.amount < rhs.amount
+//    }
+//
+//    var color: Colors
+//    var amount: Int
+//
+//    init(_ outcome: (key: Colors, value: Int)) {
+//        self.color = outcome.key
+//        self.amount = outcome.value
+//    }
+//}
+//
+//struct GameOutcome: Equatable {
+//    var outcomes: [Outcome]
+//
+//    init(_ outcomes: [Colors: Int]) {
+//        self.outcomes = outcomes.map(Outcome.init)
+//    }
+//}
+//
+//extension Array where Element == Outcome {
+//    func elementsLessThanOrEqual(_ other :[Outcome]) -> Bool {
+//        allSatisfy { possible in
+//            let potential = other.first { $0.color == possible.color }
+//            return potential! <= possible
+//        }
+//    }
+//}
+//
+//let possibleGames: GameOutcome = .init([
+//    .red: 12,
+//    .green: 13,
+//    .blue: 14
+//])
+//
+//let potentialGames: GameOutcome = .init([
+//    .red: 11,
+//    .green: 13,
+//    .blue: 14
+//])
+//
+//let result = possibleGames.outcomes.allSatisfy { possible in
+//    let potential = potentialGames.outcomes.first { $0.color == possible.color }
+//    return potential! <= possible
+//}
+//
+//print(result)
+//
+//let r2 = possibleGames.outcomes.elementsLessThanOrEqual(potentialGames.outcomes)
+//print(r2)
+
